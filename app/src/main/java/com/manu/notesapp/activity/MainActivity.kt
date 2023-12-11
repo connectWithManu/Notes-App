89b7e562054160540a8c8c21f552c1d7a1850bf4
its Manu <manugmanu121@gmail.com>
2023-12-11 11:36:15 +0530

    commits

--- app/src/main/java/com/manu/notesapp/activity/MainActivity.kt
+++ app/src/main/java/com/manu/notesapp/activity/MainActivity.kt
@@ -2,11 +2,23 @@ package com.manu.notesapp.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupActionBarWithNavController
 import com.manu.notesapp.R
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        navController = findNavController(R.id.fragmentContainerView)
+
+        setupActionBarWithNavController(navController)
+    }
+
+    override fun onNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onNavigateUp()
     }
 }

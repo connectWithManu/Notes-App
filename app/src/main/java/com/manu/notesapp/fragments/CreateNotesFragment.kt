9f651b5ac1e59971f3322f9ddf3a3677791f05bf
its Manu <manugmanu121@gmail.com>
2023-12-11 13:13:31 +0530

    commits

--- app/src/main/java/com/manu/notesapp/fragments/CreateNotesFragment.kt
+++ app/src/main/java/com/manu/notesapp/fragments/CreateNotesFragment.kt
@@ -5,13 +5,18 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.viewModels
 import com.manu.notesapp.R
 import com.manu.notesapp.databinding.FragmentCreateNotesBinding
+import com.manu.notesapp.model.Notes
+import com.manu.notesapp.viewmodel.NotesViewModel
 import java.util.Calendar
 
 
 class CreateNotesFragment : Fragment() {
     private val binding by lazy { FragmentCreateNotesBinding.inflate(layoutInflater) }
+    val viewModel: NotesViewModel by viewModels()
     var priority: String = "1"
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -32,16 +37,19 @@ class CreateNotesFragment : Fragment() {
         }
 
         binding.red.setOnClickListener {
+            priority = "3"
             binding.red.setImageResource(R.drawable.baseline_done_24)
             binding.yellow.setImageResource(0)
             binding.green.setImageResource(0)
         }
         binding.green.setOnClickListener {
+            priority = "1"
             binding.green.setImageResource(R.drawable.baseline_done_24)
             binding.yellow.setImageResource(0)
             binding.red.setImageResource(0)
         }
         binding.yellow.setOnClickListener {
+            priority = "2"
             binding.yellow.setImageResource(R.drawable.baseline_done_24)
             binding.red.setImageResource(0)
             binding.green.setImageResource(0)
@@ -49,10 +57,14 @@ class CreateNotesFragment : Fragment() {
     }
 
     private fun createNotes(it: View?) {
-        val title = binding.entertitle.text
-        val subTitle = binding.entersub.text
-        val note = binding.enternotes.text
-        val date = Calendar.getInstance().time
+        val title = binding.entertitle.text.toString()
+        val subTitle = binding.entersub.text.toString()
+        val note = binding.enternotes.text.toString()
+        val date = Calendar.getInstance().time.toString()
+
+        val notes = Notes(null, title, subTitle, note,date, priority)
+        viewModel.addNotes(notes)
+        Toast.makeText(requireContext(), "Notes Created Successfully", Toast.LENGTH_SHORT).show()
 
 
     }
--- app/src/main/java/com/manu/notesapp/room/NotesDatabase.kt
+++ app/src/main/java/com/manu/notesapp/room/NotesDatabase.kt
@@ -1,9 +1,12 @@
 package com.manu.notesapp.room
 
 import android.content.Context
+import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import com.manu.notesapp.model.Notes
 
+@Database(entities = [Notes::class], version = 1, exportSchema = false)
 abstract class NotesDatabase: RoomDatabase() {
     abstract fun myNotesDao(): NotesDao
 
